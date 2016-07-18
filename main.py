8f7a28bff8a83b1c41820afa129c7428d349c482
Bryce Carter <bryce.carte@gmail.com>
2016-07-17 22:16:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def handle_cli_input(default_kwargs):
 	kwargs = default_kwargs.copy()
 
 	try:
-		f_args = map(lambda f_arg: f_arg.split("="), sys.argv[1:])
-		for key, val in f_args:
+		for key, val in map(lambda f_arg: f_arg.split("="), sys.argv[1:]):
 			assert(key in default_kwargs.keys())
 			kwargs[key] = eval(val)
 
@@ -35,11 +34,10 @@ def main():
 	screen = pygame.Surface((SIZE, SIZE))
 	screen.fill((45,45,45))
 
-	tree = Tree(**kwargs)
-	tree.build(60)
-	tree.draw_triangles(screen, (255,255,255), filled=True, aalias=True)
-	pygame.image.save(screen, "circle-genart-{}.png".format(
-		int(time.time())))
+	t = Tree(**kwargs)
+	t.build(60)
+	t.draw_triangles(screen, (255,255,255), filled=True, aalias=True)
+	pygame.image.save(screen, "circle-genart-{}.png".format(int(time.time())))
 
 if __name__ == "__main__":
 	main()
