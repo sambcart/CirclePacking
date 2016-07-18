007703fd1bb7909709fca8e55005329d6e06c7a6
Bryce Carter <bryce.carte@gmail.com>
2016-07-17 21:28:14 -0400

    Create node.py

---
+++ node.py
@@ -0,0 +1,21 @@
+class Node(object):
+
+	def __init__(self, (x, y), rad):
+		self.x = x
+		self.y = y
+		self.pos = (x, y)
+		self.rad = rad
+
+	def distance(self, node):
+		dx = self.x - node.x
+		dy = self.y - node.y
+		return (dx * dx + dy * dy) ** 0.5
+
+	def intersect(self, node):
+		return self.distance(node) <= self.rad + node.rad - 1		
+
+	@staticmethod
+	def center_wrt_surf(pos, surf):
+		x, y = pos
+		return map(int, ( x + surf.get_width() / 2.,
+						 -y + surf.get_height() / 2.))
